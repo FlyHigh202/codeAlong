8b1b347147c2b831b70905421ec590aca2e41b41
FlyHigh202 <hung.tp.909@aptechlearning.edu.vn>
2021-04-12 18:36:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Venus Handicrafts</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./site.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+    <php?>
+    
+    <div id="header0">
+        
+    </div>
+     <div id="header">
+        <div class="venus">
+            <img src="ver.png" width="250px" height="80px" >
+        </div>
+
+        <div class="c">
+            <button class="buttonheader" type="button" 
+            class="btn btn-primary btn-lg" 
+            background-color: transparent; >  
+                <a href="index.php">Home</a>                     
+            </button>  
+        </div>
+            <button class="buttonheader" type="button"
+            class="btn btn-primary btn-lg" 
+            background-color: transparent; 
+            style="margin-left: 1%;" >
+                <a href="about.php"> About</a>
+            </button>
+
+            <button class="buttonheader" type="button" 
+            class="btn btn-primary btn-lg" 
+            background-color: transparent; 
+            style="margin-left: 1%;">
+                <a href="Gallery.php"> Gallery</a>
+            </button>     
+        </div >
+        <div class="container">   
+        <div class="body1" >
+        <center>
+        <table>
+            <tr>
+                <td style="font-size:30px; padding-left:2%;padding-right:2%" >
+            Handicrafts are smart choice<br>
+            to make your home more beautiful.  
+                </td>
+                <td >
+            <img src="12.jpg" 
+            width="700px" height="400px" style="margin-top:2%; padding-left=1%">
+            </td> 
+            </tr>
+        </table>   
+        </div>
+       </php>
+       <php> 
+        </center>
+
+        <div class="body2" >
+             
+        <center>
+        
+        <h  >THE UTILITIES WE BRING TO YOU </h>
+        </p>
+        <table>
+            <tr >
+
+                <td >
+                <img src="13.jpeg" width="250px" >  
+                </td>
+
+                <td >
+                    <img src="14.jpg" width="250px" style="margin-left:5%">
+                </td>
+
+                <td >
+                    <img src="BrassHandicrafts.jpg" width="250px" style="margin-left:10%">
+                </td>
+
+                <td >
+                    <img src="15.jpg" width="250px" style="margin-left:15%">
+                </td>  
+            </tr>
+
+            <tr>
+                <td>
+                <button type="button"  
+                class="neu"
+                style="margin-left:15%; margin-top:1%"> <a href="ironhandicraft.php">Iron Handicrafts<a></button>
+                </td>
+
+                <td>
+                <button type="button" class="neu" 
+                style="margin-left:20%; margin-top:1%"> <a href="glasshandicraft.php">Glass Handicrafts</button>
+                </td>
+
+                <td>
+                <button type="button" class="neu" 
+                style="margin-left:22%; margin-top:1%"> <a href="brasshandicraft.php">Brass Handicrafts</button>
+                </td>
+
+                <td>
+                <button type="button" class="neu" 
+                style="margin-left:22.5%; margin-top:1%"> <a href="woodhandicraft.php">Wood Handicrafts</button>
+                </td>
+            </tr>
+            
+            <tr >
+
+                <td >
+                <img src="16.jpg" width="250px" >  
+            </td>
+
+            <td >
+                <img src="17.jpg" width="250px" style="margin-left:5%">
+            </td>
+
+            <td >
+                 <img src="18.jpg" width="250px" style="margin-left:10%">
+            </td>
+
+            <td >
+                <img src="19.jpg" width="250px" style="margin-left:15%">
+            </td>  
+            </tr>
+            <tr>
+                <td>
+                <button type="button" class="neu" 
+                style="margin-left:15%; margin-top:1%"><a href="aluminiumhandicrafts.php">Aluminium Handicrafts</button>
+                </td>
+
+                <td>
+                <button type="button" class="neu" 
+                style="margin-left:20%; margin-top:1%"><a href="Tableswareshandicrafts.php">Tables wares</button>
+                </td>
+
+                <td>
+                <button type="button" class="neu" 
+                style="margin-left:22%; margin-top:1%"><a href="Homedecor.php">Home Décor</button>
+                </td>
+
+                <td>
+                <button type="button" class="neu" 
+                style="margin-left:22.5%; margin-top:1%"><a href="candleaccessories.php">Candle Accessories</button>
+                </td>
+            </tr>
+</table>
+</center>
+</div>
+
+
+    <div class="bodyy">
+        <center >
+       Venus Handicrafts   <br>--------------<br>
+       BRING THE ESSENCE TO YOUR HOME.  
+
+
+    </center>
+      </div>
+     </div>
+
+        
+</php>
+        
+    
+</body>
+</html>
